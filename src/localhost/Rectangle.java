807bcc42fe000fcf973eecf9f177683294243767
Oboist <englishhorn@mail.ru>
2018-08-16 14:43:21 +0300

    Removed redundant .this keywords and x1, y1, x2, y2 variables that had no usage outside constructor. Getters are 'private' now.

--- src/localhost/Rectangle.java
+++ src/localhost/Rectangle.java
@@ -1,58 +1,64 @@
 package localhost;
 
 class Rectangle {
-    private double x1, y1, x2, y2, minX, maxX, minY, maxY;
-
+    private double minX, maxX, minY, maxY;
+    /*
+    New Rectangle constructor. Also calculates maximum and minimum coordinates on both axis.
+     */
     Rectangle(double x1, double y1, double x2, double y2) {
-        this.x1 = x1;
-        this.y1 = y1;
-        this.x2 = x2;
-        this.y2 = y2;
 
-        this.minX = Math.min(x1, x2);
-        this.minY = Math.min(y1, y2);
+        minX = Math.min(x1, x2);
+        minY = Math.min(y1, y2);
 
-        this.maxX = Math.max(x1, x2);
-        this.maxY = Math.max(y1, y2);
+        maxX = Math.max(x1, x2);
+        maxY = Math.max(y1, y2);
 
     }
 
     double getSize() {
 
-        return (this.maxX - this.minX) * (this.maxY - this.minY);
+        return (maxX - minX) * (maxY - minY);
+
     }
 
-    double getMinX() {
+    private double getMinX() {
 
-        return this.minX;
+        return minX;
     }
 
-    double getMinY() {
+    private double getMinY() {
 
-        return this.minY;
+        return minY;
     }
 
-    double getMaxX() {
+    private double getMaxX() {
 
-        return this.maxX;
+        return maxX;
     }
 
-    double getMaxY() {
+    private double getMaxY() {
 
-        return this.maxY;
+        return maxY;
     }
 
+    /*
+    Returns new Rectangle instance - overlap area of two parent rectangles.
+    Please use 'overlaps' check first!
+     */
     Rectangle getOverlapArea(Rectangle other) {
         double newX1, newY1, newX2, newY2;
 
-        newX1 = Math.min(this.maxX, other.getMaxX());
-        newX2 = Math.max(this.minX, other.getMinX());
-        newY1 = Math.min(this.maxY, other.getMaxY());
-        newY2 = Math.max(this.minY, other.getMinY());
+        newX1 = Math.min(maxX, other.getMaxX());
+        newX2 = Math.max(minX, other.getMinX());
+        newY1 = Math.min(maxY, other.getMaxY());
+        newY2 = Math.max(minY, other.getMinY());
 
         return new Rectangle(newX1, newY1, newX2, newY2);
     }
 
+    /*
+    Check if provided Rectangle overlaps current Rectangle by comparing y and x coordinates of their corners.
+     */
     boolean overlaps(Rectangle other) {
         boolean xOverlaps = (other.getMaxX() <= maxX && other.getMaxX() >= minX) || (other.getMinX() <= maxX && other.getMinX() >= minX) || (other.getMinX() >= minX && other.getMaxX() <= maxX) || (other.getMinX() <= minX && other.getMaxX() >= maxX);
         boolean yOverlaps = (other.getMaxY() <= maxY && other.getMaxY() >= minY) || (other.getMinY() <= maxY && other.getMinY() >= minY) || (other.getMinY() >= minY && other.getMaxY() <= maxY) || (other.getMinY() <= minY && other.getMaxY() >= maxY);
